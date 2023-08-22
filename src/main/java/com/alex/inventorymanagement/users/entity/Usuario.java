de10574c3adb87573d3a6f1ebd081f92bc1cb1c1
Adrian Changalombo <alextravc.342@gmail.com>
2023-08-22 09:30:29 -0500

    [ADD]: ManyToMany between user and role

--- src/main/java/com/alex/inventorymanagement/users/entity/Usuario.java
+++ src/main/java/com/alex/inventorymanagement/users/entity/Usuario.java
@@ -1,9 +1,11 @@
 package com.alex.inventorymanagement.users.entity;
 
+import com.alex.inventorymanagement.auth.entity.Role;
 import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 
 @Data   // provee todos los getter/setter, toString, @RequiredArgsConstructor
@@ -27,6 +29,15 @@ public class Usuario {
     private String password;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> roles;
 
 
     @PrePersist
