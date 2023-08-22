1a1472ffcbbbb0ece9d2958fd0ce9e0ad4e0347f
Adrian Changalombo <alextravc.342@gmail.com>
2023-08-22 09:44:09 -0500

    [ADD]: UserDetailsImpl

---
+++ src/main/java/com/alex/inventorymanagement/auth/jwt/UserDetailsImpl.java
@@ -0,0 +1,63 @@
+package com.alex.inventorymanagement.auth.jwt;
+
+import com.alex.inventorymanagement.auth.entity.Role;
+import com.alex.inventorymanagement.users.entity.Usuario;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+@RequiredArgsConstructor     // Inject all final properties by constructor
+public class UserDetailsImpl implements UserDetails {
+
+    private final Usuario user;
+
+
+    // Roles
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return mapRoles(user.getRoles());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();     // with we want to work (email, uuid, username)
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+    // map all user roles
+    private Collection<? extends GrantedAuthority> mapRoles(Set<Role> roles) {
+        return roles.stream().map(rol -> new SimpleGrantedAuthority(rol.getName())).collect(Collectors.toList());
+    }
+
+}
