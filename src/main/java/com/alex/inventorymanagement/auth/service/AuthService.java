d0e298f0207e8e686c51482814741887d2660c9b
Adrian Changalombo <alextravc.342@gmail.com>
2023-08-22 14:21:58 -0500

    [ADD]: Initial Auth Service

---
+++ src/main/java/com/alex/inventorymanagement/auth/service/AuthService.java
@@ -0,0 +1,14 @@
+package com.alex.inventorymanagement.auth.service;
+
+import com.alex.inventorymanagement.auth.dto.AuthResponseDto;
+import com.alex.inventorymanagement.auth.dto.LoginRequestDto;
+import com.alex.inventorymanagement.auth.dto.RegisterRequestDto;
+
+
+public interface AuthService {
+
+    AuthResponseDto register(RegisterRequestDto registerDto);
+
+    AuthResponseDto login(LoginRequestDto loginDto);
+
+}
