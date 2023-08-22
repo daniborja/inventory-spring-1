7f8f98627efc980ac9b6b97a9169baa7b1c4af58
Adrian Changalombo <alextravc.342@gmail.com>
2023-08-22 12:29:12 -0500

    [ADD]: handleMethodArgumentNotValid for GlobalExceptionHandler

--- src/main/java/com/alex/inventorymanagement/common/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/alex/inventorymanagement/common/exceptions/GlobalExceptionHandler.java
@@ -1,14 +1,21 @@
 package com.alex.inventorymanagement.common.exceptions;
 
 import com.alex.inventorymanagement.common.dto.ErrorDetails;
+import lombok.NonNull;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 // Handler de TODAS las exceptions de nuestra App. -- basta con esto, ya NOO necesita mas nada, ni ser importado ni notificado a Spring
@@ -28,6 +35,25 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {  //
                 .build();
 
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    // // Spring Validation (@Valid): handler de los errors del @Valid <- BindingResult
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            @NonNull HttpHeaders headers,
+            @NonNull HttpStatusCode status,
+            @NonNull WebRequest request
+    ) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(err -> {
+            String fieldName = ((FieldError) err).getField();
+            String message = err.getDefaultMessage();
+
+            errors.put(fieldName, message);
+        });
+
+        return new ResponseEntity<>(errors, status);
     }
 
 }
