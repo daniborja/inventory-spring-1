7502078e7cb4861a373eaae01be6cf13aef352cb
Adrian Changalombo <alextravc.342@gmail.com>
2023-08-29 14:22:04 -0500

    [ADD]: Address Entity (User OneToMany Address)

---
+++ src/main/java/com/alex/inventorymanagement/addresses/entity/Address.java
@@ -0,0 +1,41 @@
+package com.alex.inventorymanagement.addresses.entity;
+
+import com.alex.inventorymanagement.users.entity.Usuario;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.Where;
+
+
+@Data
+@Entity
+@Table(name = "address")
+@Where(clause = "deleted = false") // filtra los deleted para todos los Select
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String city;
+
+    private String province;
+    private String zipCode;
+    private String phone;
+    protected boolean deleted;
+
+    @Column(nullable = false)
+    private String mainStreet;
+    private String secondaryStreet;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private Usuario user;
+
+}
