905ac279165a2e409e778c0982579a21fa4d9906
Adrian Changalombo <alextravc.342@gmail.com>
2023-08-29 15:45:42 -0500

    [ADD]: OrderItem Entity (Order OneToMany OrderItem)

---
+++ src/main/java/com/alex/inventorymanagement/orders/entity/OrderItem.java
@@ -0,0 +1,44 @@
+package com.alex.inventorymanagement.orders.entity;
+
+import com.alex.inventorymanagement.products.entity.Product;
+import com.alex.inventorymanagement.products.entity.ProductMeasurement;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+
+@Data
+@Entity
+@Table(name = "order_item")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private BigDecimal priceAtPurchase;
+
+
+    @ManyToOne
+    @JsonIgnore
+    @JsonBackReference("order_orderitem_ref")
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    @JsonBackReference("product_orderitem_ref")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "measurement_id")
+    @JsonBackReference("measurement_orderitem_id")
+    private ProductMeasurement productMeasurement;  // product&measurement = unique
+
+}
