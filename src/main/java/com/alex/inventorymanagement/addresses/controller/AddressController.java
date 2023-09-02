90f9cd0180c462b81d88c51dcbb9db81686281f8
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-01 22:24:31 -0500

    [ADD]: AddressController (findAllByAuthUser)

--- src/main/java/com/alex/inventorymanagement/addresses/controller/AddressController.java
+++ src/main/java/com/alex/inventorymanagement/addresses/controller/AddressController.java
@@ -3,6 +3,7 @@ package com.alex.inventorymanagement.addresses.controller;
 import com.alex.inventorymanagement.addresses.dto.AddressRequestDto;
 import com.alex.inventorymanagement.addresses.dto.AddressResponseDto;
 import com.alex.inventorymanagement.addresses.service.AddressService;
+import com.alex.inventorymanagement.users.entity.Usuario;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +26,13 @@ public class AddressController {
         String authUserEmail = ((UserDetails) authentication.getPrincipal()).getUsername();
 
         return new ResponseEntity<>(addressService.create(addressRequestDto, authUserEmail), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/auth-user")
+    public ResponseEntity<?> findAllByAuthUser(Authentication authentication) {
+        String authUserEmail = ((UserDetails) authentication.getPrincipal()).getUsername();
+
+        return ResponseEntity.ok(addressService.findAllByAuthUser(authUserEmail));
     }
 
     @GetMapping("/{id}")
